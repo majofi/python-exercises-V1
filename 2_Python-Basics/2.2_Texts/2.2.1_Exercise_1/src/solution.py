ed5c52a59f79b3906cb9e16d8748df2cfbb76045
Stelios Sotiriadis <steliosot@msn.com>
2021-06-14 07:32:08 +0100

    Update solution.py

--- 2_Python-Basics/2.2_Texts/2.2.1_Exercise_1/src/solution.py
+++ 2_Python-Basics/2.2_Texts/2.2.1_Exercise_1/src/solution.py
@@ -1,1 +1,34 @@
+# Task 2.2.1 
 
+city="London"
+print(city.upper())
+
+# Task 2.2.2 
+
+city="berlin"
+population = 3645000
+print(city.capitalize()+":"+str(population))
+
+
+# Task 2.2.3 
+
+city="london"
+population = 9000000
+print("City"+":"+city.upper() +"("+str(city.isalpha())+")")
+print("Population"+":"+str(population))
+
+# Task 2.2.4 
+
+text="Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital. "
+print(text.find("capital"))
+
+
+# Task 2.2.5
+
+text="Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital. "
+print(text.split())
+
+# Task 2.2.6
+
+text="Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital. "
+print(text.replace("capital","capital of Germany"))
